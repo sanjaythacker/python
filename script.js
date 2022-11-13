8f1b15cd791ec37a6817d002ab88ee6388a3c6bc
Sanjay N Thacker <sanjaythacker@hotmail.com>
2022-11-13 14:44:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,4 @@ function endsWith(x, y) {
     return x.lastIndexOf(y) === x.length - y.length;
 }
 
-// define database and app
-const pg = require ('pg');
-const app = require('express')();
-const pool = new pg.Pool();
 
-// This will produce a SQL injection and rate limiting alerts
-// Get a category from the database
-app.get("/category/:category", function (req, res) {
-
-    //Bad: the category might have SQL special characaters in it
-    var query1 = "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='"
-                + req.params.category +
-                "' ORDER BY PRICE";
-
-    pool.query(query1, [], function(err, results) {
-        // process results
-    });
-
-    res.send('Results')
-
-});
